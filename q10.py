2cb8513e1f7dc02598cb9dca99ffeff7f1895c4a
YellenoVilanova <yellenno.vilanova@gmail.com>
2025-04-16 08:45:54 -0300

    Update q10.py
    Resposta Q10

--- q10.py
+++ q10.py
@@ -1,1 +1,7 @@
-
+salario_mensal = 5000
+inss = (salario_mensal) * 15/100
+ir = (salario_mensal) * 10/100
+salario_liquido = salario_mensal - ir - inss
+print(inss)
+print(ir)
+print(salario_liquido)
